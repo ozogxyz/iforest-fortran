4caf93833af2f50164758b1e5d29348234eebb5d
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2025-07-12 18:09:55 +0300

    update license

--- main.f90
+++ main.f90
@@ -1,7 +1,7 @@
 !------------------------------------------------------------------------------
 ! MIT License
 !
-! Copyright (c) 2025 Your Name
+! Copyright (c) 2025 Orkun Ozoglu
 !
 ! Permission is hereby granted, free of charge, to any person obtaining a copy
 ! of this software and associated documentation files (the "Software"), to deal
